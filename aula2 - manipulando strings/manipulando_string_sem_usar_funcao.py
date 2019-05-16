030cf79bf5585fe329af07a0a70ee237c4c1f016
H Jay Simpson <hjaysimpsoncoorp@gmail.com>
2019-05-16 11:23:08 -0300

    exemplo de manipulação de String sem utilizar funções

---
+++ aula2 - manipulando strings/manipulando_string_sem_usar_funcao.py
@@ -0,0 +1,4 @@
+palavra = "cobra d'água" #usando aspas duplas podemos fazer uso de apóstrofe
+palavra_invertida = palavra[::-1] #invertendo a palavra
+parte_final = palavra_invertida[7:12] 
+print(parte_final)
